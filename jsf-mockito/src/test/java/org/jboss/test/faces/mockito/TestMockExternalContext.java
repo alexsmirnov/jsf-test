7e7019caafda55538aac6dc1b796cceaef38f14a
Lukas Fryc <lfryc@redhat.com>
2011-11-17 19:56:55 +0100

    added externalContextShouldBeAbleToBeStubbed test

--- jsf-mockito/src/test/java/org/jboss/test/faces/mockito/TestMockExternalContext.java
+++ jsf-mockito/src/test/java/org/jboss/test/faces/mockito/TestMockExternalContext.java
@@ -1,5 +1,6 @@
 package org.jboss.test.faces.mockito;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
@@ -9,6 +10,7 @@ import javax.faces.context.FacesContext;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 public class TestMockExternalContext {
 
@@ -38,5 +40,14 @@ public class TestMockExternalContext {
         assertTrue(environment.getFacesContext().getExternalContext() instanceof ExternalContext);
         assertTrue(FacesContext.getCurrentInstance().getExternalContext() instanceof ExternalContext);
         assertSame(environment.getFacesContext().getExternalContext(), FacesContext.getCurrentInstance().getExternalContext());
+    }
+
+    @Test
+    public void externalContextShouldBeAbleToBeStubbed() {
+        environment.withExternalContext();
+        ExternalContext externalContext = environment.getExternalContext();
+        Mockito.when(externalContext.getAuthType()).thenReturn("customAuthType");
+
+        assertEquals("customAuthType", FacesContext.getCurrentInstance().getExternalContext().getAuthType());
     }
 }
